e2d22050d80ae7efe9df82b12e679a20872fa1b2
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-14 21:44:38 +0600

    Add files via upload

---
+++ Age_Calculation/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Age_Calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(267, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Tuesday;
+            this.monthCalendar1.Location = new System.Drawing.Point(155, 53);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowTodayCircle = false;
+            this.monthCalendar1.ShowWeekNumbers = true;
+            this.monthCalendar1.TabIndex = 2;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 448);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age_Calculation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ Age_Calculation/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Calculation
+{
+    public partial class Form1 : Form
+    {
+        int age;
+        bool unvalidDate = false;
+        DateTime selectedDate;
+        DateTime currentDate = DateTime.Today;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromKnownColor(KnownColor.Azure);
+        }
+        private int CalculateAge(DateTime birthDate)
+        {
+            int age = currentDate.Year - birthDate.Year;
+
+            if (birthDate > currentDate.AddYears(-age) )
+                age--;
+
+            return age;
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            selectedDate = monthCalendar1.SelectionStart;
+
+
+            if (selectedDate >= currentDate)
+            {
+                unvalidDate = true;
+                MessageBox.Show("Please select a valid Date first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+                unvalidDate = false;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selectedDate == DateTime.MinValue)
+            {
+                MessageBox.Show("Please select a date of birth first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (unvalidDate == false)
+            {
+                age = CalculateAge(selectedDate);
+                MessageBox.Show($"You are {age} years old.", "Age");
+            }
+            else
+                MessageBox.Show("Error", "Error....");
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to close the application?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                this.FormClosing -= Form1_FormClosing;
+                Application.Exit();
+            }
+            else
+                e.Cancel = true;
+        }
+    }
+}
