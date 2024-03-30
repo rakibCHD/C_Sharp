6344560c4dbc85e59294861a57aa078a2049be14
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-30 10:50:42 +0600

    Update AllNotification.cs

--- Freelancing_Marketplace/WindowsFormsApp1/AllNotification.cs
+++ Freelancing_Marketplace/WindowsFormsApp1/AllNotification.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+ 
 namespace WindowsFormsApp1
 {
     public partial class AllNotification : Form
