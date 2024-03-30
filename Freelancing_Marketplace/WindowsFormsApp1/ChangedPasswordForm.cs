b7bbb54e0dde9b4e3a948ce7495f6f9a47cca688
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-03-30 10:51:13 +0600

    Update ChangedPasswordForm.cs

--- Freelancing_Marketplace/WindowsFormsApp1/ChangedPasswordForm.cs
+++ Freelancing_Marketplace/WindowsFormsApp1/ChangedPasswordForm.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+ 
 namespace WindowsFormsApp1
 {
     public partial class ChangedPasswordForm : Form
