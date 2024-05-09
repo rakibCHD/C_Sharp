63e0360a3e0aaca39fe4f59c812dc93603996be2
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-05-09 22:46:56 +0600

    Add files via upload

---
+++ Arduino_Control/Gvar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arduino_Control
+{
+    class Gvar
+    {
+        public static bool isDhexUploaded = false;
+        public static bool isNewHexUpload = false;
+        public static int count=0;
+        public static string AvrdudeExePath;
+        public static string AvrConfigPath;
+    }
+}
