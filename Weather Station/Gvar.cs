401c08cd900495a69b030cf252e44a9a705f1198
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-05-31 18:57:01 +0600

    Add files via upload

---
+++ Weather Station/Gvar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Station
+{
+    class Gvar
+    {
+        public static string ExePath;
+        public static string ConfigPath;
+    }
+}
